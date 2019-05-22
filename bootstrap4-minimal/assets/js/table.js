70a3e8a98af77e1b648145412b5e42abb28a7333
Nick <ncduncan@umail.ucsb.eduncduncan@umail.ucsb.edu>
2019-05-22 14:39:16 -0700

    Tables are sorted alphabetically

--- bootstrap4-minimal/assets/js/table.js
+++ bootstrap4-minimal/assets/js/table.js
@@ -39,6 +39,9 @@ jQuery(document).ready(function($) {
 	}, function() {
 	    $(this).removeClass('table-hover');
 	});
+	$.each($("table"), function(){
+		sortTable($(this), 'asc');
+	})
 });
 
 // Search the tables on the page for the input to the search bar
@@ -69,3 +72,16 @@ $(document).ready(function(){
 
   });
 });
+
+
+function sortTable(table, order) {
+    var asc   = order === 'asc',
+        tbody = $(table).find('tbody');
+    tbody.find('tr').sort(function(a, b) {
+        if (asc) {
+            return $('td:first', a).text().localeCompare($('td:first', b).text());
+        } else {
+            return $('td:first', b).text().localeCompare($('td:first', a).text());
+        }
+    }).appendTo(tbody);
+}
