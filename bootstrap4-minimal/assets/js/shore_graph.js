d969d0489a3fc00df32c7c0ad75a6ae13310f615
Wei Tung Chen <weitungchen1998@gmail.com>
2020-04-23 01:46:52 -0700

    changed graphing library and finished most of Stearns Wharf graph

--- bootstrap4-minimal/assets/js/shore_graph.js
+++ bootstrap4-minimal/assets/js/shore_graph.js
@@ -1,101 +1,216 @@
-const stearns_wharf_csv = `https://erddap.sccoos.org/erddap/tabledap/autoss.csv?time,pressure,pressure_flagPrimary,temperature,temperature_flagPrimary,chlorophyll,chlorophyll_flagPrimary,salinity,salinity_flagPrimary&station=%22stearns_wharf%22&time%3E=2019-01-21T08:00:00.000Z&time%3C${ new Date().toJSON() }&orderBy(%22time%22)`;
-const layout = {
-  title: 'Automated Shore Stations - Timeline',
-  xaxis: {
-    autorange: true,
-    rangeselector: {buttons: [
-      {
-          count: 1,
-          label: '1 Day',
-          step: 'day',
-          stepmode: 'backward'
-        },
-        {
-          count: 7,
-          label: '1 Week',
-          step: 'day',
-          stepmode: 'backward'
-        },
-     {
-          count: 1,
-          label: '1 Month',
-          step: 'month',
-          stepmode: 'backward'
-        },
-        {
-          count: 3,
-          label: '3 Months',
-          step: 'month',
-          stepmode: 'backward'
-        }/* , */   
-       /*  {
-          count: 6,
-          label: '6 months',
-          step: 'month',
-          stepmode: 'backward'
-        } 
-         
-        {
-          step: 'all'
-        }
-          */
-      ]},
-    type: 'date'
-  },
-  yaxis: {
-    autorange: true,
-    type: 'linear'
-  }
+const CSV_FILE = '/assets/test.csv';
+
+var units_data = {
+    pressure: 'Decibars',
+    temperature: '°C',
+    chlorophyll: '&mu;g / Liter',
+    salinity: 'PSU',
 };
 
+var time_data = [];
+var pressure_data = [];
+var temperature_data = [];
+var chlorophyll_data = [];
+var salinity_data = [];
 
-Plotly.d3.csv(stearns_wharf_csv, async function(err, rows) {
+var days = 0;
 
-  function unpack(rows, key) {
-    return rows.map(function(row) { return row[key]; });
-  }
+var margin = {top: 30, right: 20, bottom: 60, left: 50};
+var width = $(window).width() * 0.8 - margin.left - margin.right;
+var height = $(window).height() * 0.7 - margin.top - margin.bottom;
 
-  var pressure = {
-    type: "scatter",
-    mode: "lines",
-    name: 'Pressure, decibars',
-    x: unpack(rows, 'time'),
-    y: unpack(rows, 'pressure'),
-    line: {color: '#dd00dd'} /* magenta */
+var parseDate = d3.utcParse("%Y-%m-%dT%H:%M:%SZ");
 
-  }
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
-  var temp = {
-    type: "scatter",
-    mode: "lines",
-    name: 'Temperature, degrees Celsius (coming soon: unit option',
-    x: unpack(rows, 'time'),
-    y: unpack(rows, 'temperature'),
-    line: {color: '#00dddd'} /* cyan */
+var xAxis = d3.axisBottom(x).ticks(10);
+var yAxis = d3.axisLeft(y).ticks(10);
+var valueline = d3.line().x(d => x(d.x)).y(d => y(d.y));
 
-  }
+var svg = d3.select("#shore-graph")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var chl = {
-    type: "scatter",
-    mode: "lines",
-    name: 'Chlorophyll, ~ &mu;g/liter',
-    x: unpack(rows, 'time'),
-    y: unpack(rows, 'chlorophyll'),
-    line: {color: '#00dd00'} /* green */
-  }
+var div = d3.select("#shore-graph").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-  var sal = {
-    type: "scatter",
-    mode: "lines",
-    name: 'Salinity, PSU (~ ppt)',
-    x: unpack(rows, 'time'),
-    y: unpack(rows, 'salinity'),
-    line: {color: '#ff8800'} /* orange */
-  }
+svg.append("path").attr("id", "pressure-line").attr("class", "line");
+svg.append("path").attr("id", "temperature-line").attr("class", "line");
+svg.append("path").attr("id", "chlorophyll-line").attr("class", "line");
+svg.append("path").attr("id", "salinity-line").attr("class", "line");
 
-  var data = [temp, chl, pressure, sal];
-  Plotly.newPlot('shore-graph', data, layout);
-  $(".modebar").attr("hidden", "hidden");
-})
+svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")");
+svg.append("g").attr("class", "y axis");
 
-Plotly.newPlot('shore-graph', [], layout);
+// svg.append("text").attr("transform", "translate(" + (width / 2) + ", " + (height + margin.top + 20) + ")")
+//     .style("text-anchor", "middle")
+//     .text("Time");
+
+// svg.append("text")
+//     .attr("transform", "rotate(-90)")
+//     .attr("y", 0 - margin.left)
+//     .attr("x", 0 - (height / 2))
+//     .attr("dy", "1em")
+//     .style("text-anchor", "middle")
+//     .text("Value"); 
+
+
+svg.append("g").attr("class", "grid").attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).ticks(10).tickSize(-height).tickFormat(''));
+
+svg.append("g").attr("class", "grid")
+    .call(d3.axisLeft(y).ticks(10).tickSize(-width).tickFormat(''));
+
+
+d3.csv(CSV_FILE).then(data => {
+    data.shift();
+
+    data.forEach((d) => {
+        let time = parseDate(d.time);
+
+        time_data.push          (time);
+        pressure_data.push      ({ x: time, y: d.pressure });
+        temperature_data.push   ({ x: time, y: d.temperature });
+        chlorophyll_data.push   ({ x: time, y: d.chlorophyll });
+        salinity_data.push      ({ x: time, y: d.salinity });
+    });
+
+    updateData($('#graph-time-range #one-month'), 30);
+});
+
+async function updateData(target, _days) {
+    days = _days;
+    udpateGraphData();
+
+    $('#graph-time-range .btn').removeClass('btn-primary');
+    $(target).addClass('btn-primary');
+    $('#current-time').text(formatTime(new Date()));
+}
+
+function udpateGraphData() {
+    let end_date = time_data[time_data.length - 1];
+    let start_date = new Date(end_date - 1000 * 60 * 60 * 24 * days);
+
+    let start_i = time_data.findIndex(time => time > start_date);
+    let end_i = time_data.findIndex(time => time > end_date);
+
+    if (end_i == -1) end_i = time_data.length;
+
+    let min_x = end_date, max_x = start_date, min_y = 0, max_y = 0;
+    let i = start_i;
+    for (; i < end_i; i++) {
+        max_y = Math.max(max_y, pressure_data[i].y, temperature_data[i].y, chlorophyll_data[i].y, salinity_data[i].y);
+        min_x = Math.min(min_x, time_data[i]);
+        max_x = Math.max(max_x, time_data[i]);
+    }
+
+    x.domain([min_x, max_x]);
+    y.domain([min_y, max_y + (max_y - min_y) / 10]);
+
+    let svgt = svg.transition();
+
+    svgt.select("#pressure-line")    .duration(500).attr("d", valueline(pressure_data    .slice(start_i, end_i)));
+    svgt.select("#temperature-line") .duration(500).attr("d", valueline(temperature_data .slice(start_i, end_i)));
+    svgt.select("#chlorophyll-line") .duration(500).attr("d", valueline(chlorophyll_data .slice(start_i, end_i)));
+    svgt.select("#salinity-line")    .duration(500).attr("d", valueline(salinity_data    .slice(start_i, end_i)));
+
+    svgt.select(".x.axis").duration(500).call(xAxis);
+    svgt.select(".y.axis").duration(500).call(yAxis);
+
+
+    $('.dots').remove();
+    makeDot('pressure', pressure_data    .slice(start_i, end_i));
+    makeDot('temperature', temperature_data .slice(start_i, end_i));
+    makeDot('chlorophyll', chlorophyll_data .slice(start_i, end_i));
+    makeDot('salinity', salinity_data    .slice(start_i, end_i));
+
+    function makeDot(topic, data) {
+        let sparse_data = [];
+        let inc = Math.floor(data.length / 300);
+        if (inc < 1) inc = 1;
+
+        for (let i = 0; i < data.length; i += inc) {
+            sparse_data.push(data[i]);
+        }
+
+        svg.selectAll("dot")
+            .data(sparse_data)
+            .enter()
+            .append("circle")
+                .attr("r", 7)
+                .attr("class", `dots ${topic}-dots`)
+                .attr("cx", d => x(d.x))
+                .attr("cy", d => y(d.y))
+                .attr("fill", "transparent")
+                .on("mouseover", function(d) {
+                    div.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    div.html(formatTime(d.x) + '<br/>' + parseFloat(d.y).toFixed(2) + ' ' + units_data[topic])
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 70) + "px");
+                    $(this).attr("fill", "black");
+                    $(this).css("cursor", "pointer");
+                })
+                .on("mouseout", function(d) {
+                    div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+                    $(this).attr("fill", "transparent");
+                    $(this).css("cursor", "unset");
+                });
+    }
+}
+
+async function toggleGraph(topic) {
+    $(`#${topic}-btn`).toggleClass('btn-color');
+    $(`#${topic}-line`).toggleClass('hide');
+    $(`.${topic}-dots`).toggleClass('hide');
+}
+
+function formatTime(date) {
+    return date.toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    });
+}
+
+async function toggleCelsius(turnOn) {
+    if (turnOn && units_data.temperature != '°C') {
+        units_data.temperature = '°C';
+        $(`#temperature-btn`).text('Temperature (°C)');
+
+        temperature_data.forEach(d => {
+            d.y = toCelsius(d.y);
+        });
+
+        udpateGraphData();
+    }
+    else if (!turnOn && units_data.temperature != '°F') {
+        units_data.temperature = '°F';
+        $(`#temperature-btn`).text('Temperature (°F)');
+
+        temperature_data.forEach(d => {
+            d.y = toFahrenheit(d.y);
+        });
+        udpateGraphData();
+    }
+
+}
+
+function toCelsius(f) {
+    return (f - 32) * 5 / 9;
+}
+
+function toFahrenheit(c) {
+    return c * 9 / 5 + 32;
+}
--- bootstrap4-minimal/data/visuals/stearns-wharf/index.md
+++ bootstrap4-minimal/data/visuals/stearns-wharf/index.md
@@ -2,14 +2,14 @@
 layout: article
 title: 'Current Conditions - Stearns Wharf'
 description: page description here.
-
+page_css:
+  - /assets/css/custom/data/visuals.css
 ---
 
-<h1>{{ page.title }}</h1>
 	
 	
 	 
-<div id="main-container">
+<div id="main-container" hidden>
 <div class="row">
     <div class="col-md-2">
         <p>These groups collaborate with SBC LTER on collection of data at Stearns Wharf</p>
@@ -48,11 +48,38 @@ description: page description here.
 </div>
 </div>
 
-<div class="col-md-10">
+<div id="graph-container">
+    <h1>{{ page.title }}</h1>
+
+    <div id="graph-time-range">
+        <div id="one-day"       class="btn" onclick="updateData(event.target, 1)"> 1 Day </div>
+        <div id="one-week"      class="btn" onclick="updateData(event.target, 7)"> 1 Week </div>
+        <div id="one-month"     class="btn" onclick="updateData(event.target, 31)"> 1 Month </div>
+        <div id="three-months"  class="btn" onclick="updateData(event.target, 93)"> 3 Months </div>
+
+        <div id="current-time" class="absolute-right"></div>
+    </div>
+
+    <div id="graph-lines">
+        <div id="pressure-btn"     class="btn btn-color" onclick="toggleGraph('pressure')"> Pressure (Decibars) </div>
+        <div id="temperature-btn"  class="btn btn-color" onclick="toggleGraph('temperature')"> Temperature (°C) </div>
+        <div id="chlorophyll-btn"  class="btn btn-color" onclick="toggleGraph('chlorophyll')"> Chlorophyll (&mu;g / Liter) </div>
+        <div id="salinity-btn"     class="btn btn-color" onclick="toggleGraph('salinity')"> Salinity (PSU, ~ppt) </div>
+
+        <div id="temp-change-panel" class="btn-group btn-group-toggle absolute-right" data-toggle="buttons">
+            <label class="btn btn-primary active" onclick="toggleCelsius(true)">
+                <input type="radio" name="options" autocomplete="off" checked> °C
+            </label>
+            <label class="btn btn-primary" onclick="toggleCelsius(false)">
+                <input type="radio" name="options" autocomplete="off"> °F
+            </label>
+        </div>
+    </div>
+
     <div id="shore-graph"></div>
-    
-    <p>Tide predictions for Santa Barbara: <a href="https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=9411340&legacy=1">Tides and currennts at NOAA</a>
-    </p>
+
+    <p>Tide predictions for Santa Barbara: <a href="https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=9411340&legacy=1">Tides and currennts at NOAA</a></p>
 </div>
-<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+<script src="https://d3js.org/d3.v5.min.js"></script>
 <script src="/assets/js/shore_graph.js"/></script>
