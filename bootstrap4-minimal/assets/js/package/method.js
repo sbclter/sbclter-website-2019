c659c68fb18208e339ad9a447ab072c61d312567
Wei Tung Chen <weitungchen1998@gmail.com>
2020-08-27 12:18:42 -0700

    fixed method tab to include data sources

--- bootstrap4-minimal/assets/js/package/method.js
+++ bootstrap4-minimal/assets/js/package/method.js
@@ -11,7 +11,8 @@ class PackageMethod {
 		for (let i in methodList) {
 			let methodData = {
 				descriptions: [],
-				protocols: []
+				protocols: [],
+				dataSources: [],
 			};
 
 			// Extract description list starting from most nested possible location.
@@ -20,6 +21,7 @@ class PackageMethod {
 			methodData['instrumentation'] = extractList(methodList[i], 'instrumentation');
 
 			let protocols = extractList(methodList[i], 'protocol');
+			let dataSources = extractList(methodList[i], 'dataSource');
 
 			for (let j in descriptions) {
 				methodData['descriptions'].push({
@@ -37,6 +39,15 @@ class PackageMethod {
 				});
 			}
 
+			// fill method's dataSources
+			for (let j in dataSources) {
+				methodData['dataSources'].push({
+					title: extractString(dataSources[j], 'title'),
+					creators: people.parsePeopleData(extractList(dataSources[j], 'creator')),
+					url: extractString(dataSources[j], 'distribution > online > url')
+				});
+			}
+
 			methodDataList.push(methodData);
 		}
 		this.data['protocols'] = methodDataList;
@@ -50,6 +61,7 @@ class PackageMethod {
 		for (let i in methodList) {
 			let descriptions = methodList[i]['descriptions'];
 			let protocols = methodList[i]['protocols'];
+			let dataSources = methodList[i]['dataSources'];
 			let description_html = [];
 			let protocol_html = [];
 
@@ -80,8 +92,24 @@ class PackageMethod {
 				`);
 			}
 
+			// fill method's dataSources
+			for (let j in dataSources) {
+				let dataSource = dataSources[j];
+				protocol_html.push(`
+					<table class="table">
+						<tbody>
+							${ makeTableRow([['th', 'col-2', 'Data Source:'     ], ['td', 'col-10', dataSource['title']            ]]) }
+							${ makeTableRow([['th', 'col-2', 'Creators:'        ], ['td', 'col-10', '<div class="row people-table-wrap">' +
+																										people.makePeopleTables(dataSource['creators']) +
+																									'</div>']]) }
+							${ makeTableRow([['th', 'col-2', 'Available Online:'], ['td', 'col-10', activateLink(dataSource['url'])]]) }
+						</tbody>
+					</table>
+				`);
+			}
+
 			let html = `
-				<div class="section-title"> Protocol </div>
+				<div class="section-title"> Method </div>
 				<div class="ml-3">
 					${ description_html.join('<hr>') }
 				</div>
